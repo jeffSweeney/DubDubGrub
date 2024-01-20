1328f735c0849b969cac4af656fdcf25bda9f3f9
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-20 15:29:35 -0500

    Build the LocationListView

---
+++ DubDubGrub/DubDubGrub/GrubSpotView.swift
@@ -0,0 +1,82 @@
+//
+//  GrubSpotView.swift
+//  DubDubGrub
+//
+//  Created by Jeffrey Sweeney on 1/20/24.
+//
+
+import SwiftUI
+
+struct GrubSpotView: View {
+    @State var headcount: Int // TODO: Dummy data - will replace
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 18) {
+            // MARK: - Spot Image
+            Image("maggianos") // TODO: Dummy data - will replace
+                .resizable()
+                .scaledToFill()
+                .frame(width: 85, height: 85)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading, spacing: 16) {
+                // MARK: - Spot Name
+                Text("Maggianos") // TODO: Dummy data - will replace
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                
+                // MARK: - Spot People
+                if headcount == 0 {
+                    Text("Nobody Checked In")
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.systemGray))
+                } else {
+                    spotPeopleView
+                }
+            }
+            .padding(.top, 2)
+            
+            Spacer()
+        }
+    }
+}
+
+extension GrubSpotView {
+    private var spotPeopleView: some View {
+        return HStack(spacing: 12) {
+            ForEach(1 ... min(4, headcount), id: \.self) { _ in
+                AvatarView()
+            }
+            
+            let remaining = headcount - 4
+            if remaining == 1 {
+                AvatarView()
+            } else if remaining > 0 {
+                Text("+\(remaining)")
+                    .font(.footnote)
+                    .fontWeight(.bold)
+                    .frame(width: 35, height: 35)
+                    .background(Color.brandPrimary)
+                    .clipShape(Circle())
+            }
+        }
+    }
+}
+
+#Preview {
+    GrubSpotView(headcount: Int.random(in: 0 ... 10))
+        .preferredColorScheme(.dark)
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image("default-avatar") // TODO: Dummy data - will replace
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
+    }
+}
--- DubDubGrub/DubDubGrub/LocationListView.swift
+++ DubDubGrub/DubDubGrub/LocationListView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("UNDER CONSTRUCTION: List View")
+        NavigationStack {
+            ScrollView {
+                ForEach(0...10, id: \.self) { _ in
+                    NavigationLink {
+                        Text("Under Construction")
+                    } label: {
+                        VStack {
+                            Divider()
+                            
+                            HStack {
+                                GrubSpotView(headcount: Int.random(in: 0 ... 10))
+                                    .foregroundStyle(Color.primary)
+                                
+                                Image(systemName: "chevron.right")
+                                    .imageScale(.small)
+                                    .foregroundStyle(.gray)
+                            }
+                            .padding()
+                        }
+                    }
+
+                }
+                .navigationTitle("Grub Spots")
+            }
+        }
     }
 }
 
 #Preview {
     LocationListView()
+        .preferredColorScheme(.dark)
 }
